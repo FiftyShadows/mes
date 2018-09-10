b5380093324871f2986b2ada23a1a94088122809
liuyuan <hi60@qq.com>
2018-09-10 13:26:16 +0800

    提交生产计划机台

--- src/components/auto/auto_production_plan/workshop_product_plan.vue
+++ src/components/auto/auto_production_plan/workshop_product_plan.vue
@@ -14,10 +14,7 @@
       </el-table-column>
       <el-table-column prop="batch.tubeColor" label="纸管颜色">
       </el-table-column>
-      <el-table-column prop="" label="机台">
-        <template>
-
-        </template>
+      <el-table-column prop="machines" label="机台">
       </el-table-column>
       <el-table-column prop="lineMachines.length" label="机台数">
       </el-table-column>
@@ -41,38 +38,42 @@ export default {
     }
   },
   created () {
-    this.getPlan()
+    this.getPlan();
     this.getLinePlans()
   },
   methods: {
     getPlan () { // 获取select数据
       this.$api.getWorkShopsLine().then(res => {
-        console.log(res)
-        this.LinesTable = res.data
+        console.log(res);
+        this.LinesTable = res.data;
         this.options = res.data.map(item => {
           return {name: item.name}
-        })
+        });
         console.log(this.options)
       })
     },
     getLinePlans () {
-      this.loading = true
+      this.loading = true;
       for (let i = 0; i < this.options.length; i++) {
         if (this.LinesTable[i].name === this.seachName) {
           this.seachId = this.LinesTable[i].id
         }
       }
       this.$api.getLinePlans(this.seachId).then(res => {
-        console.log(res)
-        this.allData = res.data
-        this.tableData = res.data.items
+        console.log(res);
+        this.allData = res.data;
+        this.tableData = res.data.items;
+        console.log(111);
+        console.log(this.tableData);
+        console.log(111);
         for (let j = 0; j < this.tableData.length; j++) {
-          this.MachinesNum = this.tableData[j].lineMachines
-          let arr = []
-          for (let i = 0; i < this.MachinesNum.length; i++) {
-            arr.push(this.MachinesNum[i].item)
-          }
-          console.log(arr.sort())
+          this.MachinesNum = this.tableData[j].lineMachines;
+          // let arr = []
+          // for (let i = 0; i < this.MachinesNum.length; i++) {
+          //   arr.push(this.MachinesNum[i].item)
+          // }
+          // console.log(arr.sort())
+          this.tableData[j].machines = this.tableData[j].lineMachines[0].item + '——' + this.tableData[j].lineMachines[this.tableData[j].lineMachines.length - 1].item
         }
         this.loading = false
       })
